8daf3c08d82764f306372fbf6398c933fcf43e68
Øyvind Harding Gulbrandsen <oyvindhg@gmail.com>
2017-11-10 03:27:33 +0100

    Calibration works fine, BUT! Why does the ROI go to 0 for the front camera, so that there are no valid pixels? The black area is not that large

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 #conda install -c menpo opencv3
 
+import os
 
 import cv2
 import glob
 import numpy as np
 
+current_path = os.getcwd()
+os.chdir(current_path + '/Frontcamera')
+
 image_files = glob.glob('*.jpg')
-board_size = (6,8)
+board_size = (6,9)
 
 objp = np.zeros((board_size[0]*board_size[1],3), np.float32)
 objp[:,:2] = np.mgrid[0:board_size[0],0:board_size[1]].T.reshape(-1,2)
@@ -16,14 +20,22 @@ obj_points = []
 
 h, w = 0, 0
 
+
 for image_file in image_files:
     im = cv2.imread(image_file)
+
+    im = cv2.resize(im, None, fx=0.3, fy=0.3, interpolation=cv2.INTER_CUBIC)
+
     im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     (h, w) = im_gray.shape
+
+    print(image_file)
 
     found, corners = cv2.findChessboardCorners(im_gray, board_size) #found is non-zero if all corners are found
 
     if found == True:
+
+        print('ok')
 
         # termination criteria
         term = (cv2.TERM_CRITERIA_MAX_ITER + cv2.TERM_CRITERIA_EPS, 30, 0.001)
@@ -35,9 +47,9 @@ for image_file in image_files:
         obj_points.append(objp)
 
         # Draw and display the corners
-        #img = cv2.drawChessboardCorners(im, board_size, corners, ret)
-        #cv2.imshow('img', im)
-        #cv2.waitKey(0)
+        img = cv2.drawChessboardCorners(im, board_size, corners, found)
+        cv2.imshow('img', im)
+        cv2.waitKey(0)
         #cv2.destroyAllWindows()
 
 
@@ -46,19 +58,33 @@ rms, camera_mtx, dist_coeff, rotation, translation = cv2.calibrateCamera(obj_poi
 if (rms > 1):
     print('Bad calibration. RMS error: ', rms)
 
-im = cv2.imread('right.jpg')
+im = cv2.imread('8.jpg')
+im = cv2.resize(im, None, fx=0.3, fy=0.3, interpolation=cv2.INTER_CUBIC)
 newcameramtx, roi = cv2.getOptimalNewCameraMatrix(camera_mtx, dist_coeff, (w, h), 1, (w, h))
-dst = cv2.undistort(im, camera_mtx, dist_coeff, None, newcameramtx)
+im_cal = cv2.undistort(im, camera_mtx, dist_coeff, None, newcameramtx)
 
 # crop the image. ROI are all-good pixels regions in the image
 x,y,w,h = roi
-dst = dst[y:y+h, x:x+w]
-cv2.imshow('img', dst)
+im_cal2 = im_cal[y:y+h, x:x+w]
+
+
+
+im_blend = cv2.addWeighted(im,0.5,im_cal,0.5,0)
+
+cv2.imshow('img', im_blend)
+
+cv2.imshow('orig', im)
+
+cv2.imshow('calib', im_cal)
+
+#cv2.imshow('cut', im_cal2)
 
 dst = cv2.undistort(im, camera_mtx, dist_coeff)
-
 cv2.imshow('im2', dst)
 
-
 cv2.waitKey(0)
+
+
+
+
 
